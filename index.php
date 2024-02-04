07117ee39697594eef29de091adb8730c530601f
Henry Hasibuan <henrynomail@gmail.com>
2024-02-04 14:38:24 +0700

    menambahkan paragraph

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
     <title>Coba Git Remote Kedua</title>
 </head>
 <body>
-    
+    <h1>Hello World</h1>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quaerat, rem ab dolorem pariatur quidem harum. Animi voluptas eum et a fugit quasi facere voluptates rerum, adipisci nulla quos sunt unde!</p>
 </body>
 </html>
